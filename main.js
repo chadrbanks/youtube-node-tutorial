d04fa22e1c63a77dc50564c89336c0df6512fec9
Chad Banks <chadrbanks@yahoo.com>
2020-05-21 14:52:57 -0400

    1st video checkpoint.

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let name = "Chad";
+let cash = 4.99;
+
+let strArray = ["Nodejs rules!", "", "", 34, 2.5, 2323, 'c'];
+
+let myObject = {name:"Chad", cash: cash, language: "Nodejs"}
+
+console.log("myObject:", myObject);
